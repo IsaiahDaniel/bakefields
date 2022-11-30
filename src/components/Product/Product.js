03e6e0fe8b20028c34c9aac2fd49ee97a85b38a1
Isaiah Daniel <isaiahdaniel203@gmail.com>
2022-11-30 22:47:19 +0100

    added grid-col-2 on sm

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,7 +9,7 @@ const Product = ({ product }) => {
         <img
           src={product.image}
           alt={product.title}
-          style={{ width: "300px", height: "300px" }}
+          style={{ width: "270px", height: "270px" }}
         />
       </div>
       <h3 className="font-bold">{product.title}</h3>
